40585de13d059ad035880f50cf8911f514496411
jesusherrera94 <jesusherrera1594@gmail.com>
2024-09-24 21:33:30 -0600

    integrate user inputs.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <regex>
 #include <io2d.h>
 #include "route_model.h"
 #include "render.h"
@@ -27,8 +28,27 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+bool is_number(const std::string& str) {
+    std::regex number_regex("^-?\\d*(\\.\\d+)?$");
+    return std::regex_match(str, number_regex);
+}
+
+void get_value(float& value, std::string name) {
+    bool is_valid = false;
+    do {
+        std::cout<<"Enter value for "<<name<<": ";
+        std::cin>>value;
+        if (!is_number(std::to_string(value)) || value < 0 || value > 100) {
+            std::cout<<"Invalid input. Please enter a number."<<std::endl;
+        } else {
+            is_valid = true;
+        }
+    } while (!is_valid);
+}
+
 int main(int argc, const char **argv)
 {    
+    float start_x, start_y, end_x, end_y;
     std::string osm_data_file = "";
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
@@ -55,12 +75,16 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    get_value(start_x, "start_x");
+    get_value(start_y, "start_y");
+    get_value(end_x, "end_x");
+    get_value(end_y, "end_y");
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
